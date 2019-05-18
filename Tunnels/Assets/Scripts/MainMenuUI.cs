370ea028283ecb39ed0d706deb7994a71fa5590f
sam6321 <sam6321@live.com.au>
2019-05-18 09:02:48 +0800

    Initial unity commit

---
+++ Tunnels/Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public void OnStartGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
