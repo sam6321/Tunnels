f0544e4ac86157d41318aa0bcf10b45962999f5a
sam6321 <sam6321@live.com.au>
2019-05-21 20:22:11 +0800

    Added break mask

--- Tunnels/Assets/Scripts/Tile.cs
+++ Tunnels/Assets/Scripts/Tile.cs
@@ -7,8 +7,10 @@ public class Tile : MonoBehaviour
     private int health = 10;
 
     private int startHealth;
+    private float healthLerp;
 
     private SpriteRenderer spriteRenderer;
+    private SpriteMask mask;
     private ParticleSystem.EmitParams p = new ParticleSystem.EmitParams();
 
     public ParticleSystem OnDrillParticleSystem { get; set; }
@@ -16,16 +18,24 @@ public class Tile : MonoBehaviour
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        mask = GetComponent<SpriteMask>();
         startHealth = health;
+        healthLerp = startHealth;
+    }
+
+    void Update()
+    {
+        //healthLerp = Mathf.MoveTowards(healthLerp, health, Time.deltaTime * 10.0f);
+        mask.alphaCutoff = 1 - Mathf.InverseLerp(0, startHealth, health);
     }
 
     void OnDrillAttack(DrillAttackInfo info)
     {
         health -= info.damage;
 
-        Color colour = spriteRenderer.color;
+        /*Color colour = spriteRenderer.color;
         colour.a = Mathf.InverseLerp(0, startHealth, health);
-        spriteRenderer.color = colour;
+        spriteRenderer.color = colour;*/
 
         p.position = new Vector3(0, 0, -1) + (Vector3)(info.position + info.normal * 0.01f);
         p.velocity = info.normal + Random.insideUnitCircle * 1.5f;
