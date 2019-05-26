3160144cfaecbd5f088a06bb6a23d9bfdbaade7b
Toby <toby.scantlebury@gmail.com>
2019-05-26 16:20:51 +0800

    Split up PlayerResources a bit

--- Tunnels/Assets/Scripts/PlayerResources.cs
+++ Tunnels/Assets/Scripts/PlayerResources.cs
@@ -3,86 +3,17 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 
-public enum ResourceType
-{
-    Copper = 0,
-    Silver,
-    Gold,
-    Platinum,
-    Diamond,
-
-    Iron,
-    Oil
-}
 
 [Serializable]
 public class ScoreModifiedEvent : UnityEvent<int> { }
 
-public class Resource
-{
-    public delegate void OnModify();
-
-    public readonly ResourceType type;
-    private int amount;
-    private int max;
-    private OnModify onModify;
-
-    public virtual int Amount
-    {
-        get => amount;
-        set
-        {
-            amount = Mathf.Min(value, max);
-            onModify();
-        }
-    }
-
-    public Resource(ResourceType type, OnModify onModify, int amount=0, int max=int.MaxValue)
-    {
-        this.type = type;
-        this.amount = amount;
-        this.max = max;
-        this.onModify = onModify;
-    }
-}
-
-public class ScoreResource : Resource
-{
-    private int multiplier;
-
-    public int Score => multiplier * Amount;
-
-    public ScoreResource(ResourceType type, int multiplier, OnModify onModify) : base(type, onModify)
-    {
-        this.multiplier = multiplier;
-    }
-}
-
 public class PlayerResources : MonoBehaviour
 {
-    static T[] JoinArrays<T>(params T[][] values)
-    {
-        T[] array = new T[values.Sum(v => v.Length)];
-
-        int loadPosition = 0;
-        foreach(T[] inArray in values)
-        {
-            Array.Copy(inArray, 0, array, loadPosition, inArray.Length);
-            loadPosition += inArray.Length;
-        }
-
-        return array;
-    }
-
     [SerializeField]
     private ScoreModifiedEvent onScoreModified;
     public ScoreModifiedEvent OnScoreModified { get => onScoreModified; }
 
-    // Resources stored in order so they can be accessed by enum value directly
-    private Resource[] resources;
-
-    // Resources that combine to form the players score
-    private ScoreResource[] scoreResources;
+    private ResourceManager resourceManager;
 
     private int score = 0;
     public int Score => score;
@@ -98,22 +29,7 @@ public class PlayerResources : MonoBehaviour
 
     void Start()
     {
-        scoreResources = new ScoreResource[]
-        {
-            new ScoreResource(ResourceType.Copper,   1, InternalOnScoreResourceModified),
-            new ScoreResource(ResourceType.Silver,   2, InternalOnScoreResourceModified),
-            new ScoreResource(ResourceType.Gold,     3, InternalOnScoreResourceModified),
-            new ScoreResource(ResourceType.Platinum, 6, InternalOnScoreResourceModified),
-            new ScoreResource(ResourceType.Diamond, 8, InternalOnScoreResourceModified),
-        };
-
-        Resource[] drillSpeedResources = new Resource[]
-        {
-            new Resource(ResourceType.Iron, InternalOnDrillResourceModified),
-            new Resource(ResourceType.Oil, InternalOnDrillResourceModified, amount: 100, max: 100)
-        };
-
-        resources = JoinArrays(scoreResources, drillSpeedResources);
+        resourceManager = new ResourceManager(InternalOnScoreResourceModified, InternalOnDrillResourceModified);
     }
 
     public void SetResourceCount(ResourceType type, int count)
@@ -128,12 +44,12 @@ public class PlayerResources : MonoBehaviour
 
     private T GetResource<T>(ResourceType type) where T : Resource
     {
-        return resources[(int)type] as T;
+        return resourceManager.resources[(int)type] as T;
     }
 
     private void InternalOnScoreResourceModified()
     {
-        score = scoreResources.Sum(r => r.Score);
+        score = resourceManager.scoreResources.Sum(r => r.Score);
         onScoreModified.Invoke(score);
     }
 
---
+++ Tunnels/Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum ResourceType
+{
+    Copper = 0,
+    Silver,
+    Gold,
+    Platinum,
+    Diamond,
+
+    Iron,
+    Oil
+}
+
+public class Resource
+{
+    public delegate void OnModify();
+
+    public readonly ResourceType type;
+    private int amount;
+    private int max;
+    private OnModify onModify;
+
+    public virtual int Amount
+    {
+        get => amount;
+        set
+        {
+            amount = Mathf.Min(value, max);
+            onModify();
+        }
+    }
+
+    public Resource(ResourceType type, OnModify onModify, int amount=0, int max=int.MaxValue)
+    {
+        this.type = type;
+        this.amount = amount;
+        this.max = max;
+        this.onModify = onModify;
+    }
+}
+
+public class ScoreResource : Resource
+{
+    private int multiplier;
+
+    public int Score => multiplier * Amount;
+
+    public ScoreResource(ResourceType type, int multiplier, OnModify onModify) : base(type, onModify)
+    {
+        this.multiplier = multiplier;
+    }
+}
+
+public class ResourceManager
+{
+    // Resources stored in order so they can be accessed by enum value directly
+    public Resource[] resources;
+
+    // Resources that combine to form the players score
+    public ScoreResource[] scoreResources;
+
+    static T[] JoinArrays<T>(params T[][] values)
+    {
+        T[] array = new T[values.Sum(v => v.Length)];
+
+        int loadPosition = 0;
+        foreach(T[] inArray in values)
+        {
+            Array.Copy(inArray, 0, array, loadPosition, inArray.Length);
+            loadPosition += inArray.Length;
+        }
+
+        return array;
+    }
+
+    public ResourceManager(Resource.OnModify scoreModify, Resource.OnModify drillModify)
+    {
+        scoreResources = new ScoreResource[]
+        {
+            new ScoreResource(ResourceType.Copper,   1, scoreModify),
+            new ScoreResource(ResourceType.Silver,   2, scoreModify),
+            new ScoreResource(ResourceType.Gold,     3, scoreModify),
+            new ScoreResource(ResourceType.Platinum, 6, scoreModify),
+            new ScoreResource(ResourceType.Diamond, 8, scoreModify),
+        };
+
+        Resource[] drillSpeedResources = new Resource[]
+        {
+            new Resource(ResourceType.Iron, drillModify),
+            new Resource(ResourceType.Oil, drillModify, amount: 100, max: 100)
+        };
+
+        resources = JoinArrays(scoreResources, drillSpeedResources);
+    }
+}
