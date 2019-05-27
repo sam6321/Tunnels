7217cf55ce3c21be9d3a89f53e718468fd48bf52
Toby <toby.scantlebury@gmail.com>
2019-05-27 21:12:09 +0800

    Bound camera x movement to keep more of the map on screen

--- Tunnels/Assets/Scripts/CameraMovement.cs
+++ Tunnels/Assets/Scripts/CameraMovement.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     [SerializeField]
+    private TileGenerator tileGenerator;
+
+    [SerializeField]
     private Transform transformTarget;
 
     [SerializeField]
@@ -37,7 +40,18 @@ public class CameraMovement : MonoBehaviour
         Vector3? target = GetCurrentTarget();
         if(target.HasValue)
         {
-            transform.position = Vector3.Lerp(transform.position, target.Value + offset, smoothing * Time.deltaTime);
+            Vector3 boundedTarget = target.Value;
+            Vector2 rightmost = Camera.main.ScreenToWorldPoint(new Vector2(Camera.main.pixelWidth, 0.0f));
+            Vector2 leftmost = Camera.main.ScreenToWorldPoint(new Vector2(0.0f, 0.0f));
+            Vector2 tileSize = tileGenerator.GetTileSize();
+            float xMax = tileGenerator.GetExtent().x + tileSize.x * 2;
+            float xMin = -tileSize.x * 2;
+            if((rightmost.x > xMax && boundedTarget.x > transform.position.x)
+            || (leftmost.x < xMin && boundedTarget.x < transform.position.x))
+            {
+                boundedTarget.x = transform.position.x;
+            }
+            transform.position = Vector3.Lerp(transform.position, boundedTarget + offset, smoothing * Time.deltaTime);
         }
     }
 }
